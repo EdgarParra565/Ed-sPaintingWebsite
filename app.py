b87be218731846864d743e929c794b61c64ed316
sucosPC <edgarparra2028@gmail.com>
2025-12-22 17:57:24 -0500

    added mini gallery and tried to change dbs

--- app.py
+++ app.py
@@ -27,7 +27,15 @@ app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.secret_key = os.getenv("SECRET_KEY", "fallback-secret")
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'inquiries.db')}"
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not set")
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # -----------------------------
