9b548aa4404b2337094d881b4e68d6bbf29c8232
sucosPC <edgarparra2028@gmail.com>
2025-12-22 18:00:14 -0500

    fixed limiter

--- app.py
+++ app.py
@@ -70,7 +70,8 @@ class AdminLoginForm(FlaskForm):
 limiter = Limiter(
     key_func=get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
 )
 
 # -----------------------------
